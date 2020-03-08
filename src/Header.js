951e1dd7a407f1335de78a5672fc67a2a7a4491f
Preston Doris <prestondoris@gmail.com>
2020-03-08 16:15:49 -0700

    Update Header Wording

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ const Header = props => {
   const colorCode = props.curLevel === 'EXPERT' ? <p className="header-color">{props.hex}</p> : <p className="header-color">{props.rgb}</p>
   return (
     <header className="header">
-      <p className="header-title">The Great Color Code</p>
+      <p className="header-title">The Color Code Guessing Game</p>
       {colorCode}
-      <p className="header-title">Guessing Game</p>
+      <p className="header-title">Built with Love in React!</p>
     </header>
   )
 }
